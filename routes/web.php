3a5f36cd14452a92d28b62a252a0bf48835e990d
Daan Kats <>
2025-01-14 15:29:52 +0100

    added dashboard for adding destinations - work in progress

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth'])->group(function () {
+    Route::post('/destinations', [DestinationController::class, 'store'])->name('destinations.store');
+});
 // Profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
