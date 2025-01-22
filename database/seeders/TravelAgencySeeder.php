21042597d05c1fcc7dbfcde702d7eac44b3b3262
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2025-01-22 13:09:33 +0100

    Update TravelAgencySeeder.php

--- database/seeders/TravelAgencySeeder.php
+++ database/seeders/TravelAgencySeeder.php
@@ -9,17 +9,54 @@ class TravelAgencySeeder extends Seeder
 {
     public function run()
     {
-        TravelAgency::create([
-            'name' => 'Sunshine Travel',
-            'phone' => '0612345678',
-            'email' => 'info@sunshinetravel.com',
-        ]);
-
-        TravelAgency::create([
-            'name' => 'Adventure Tours',
-            'phone' => '0623456789',
-            'email' => 'contact@adventuretours.com',
-        ]);
+        TravelAgency::create(['name' => 'Sunshine Travel', 'phone' => '0612345678', 'email' => 'info@sunshinetravel.com']);
+        TravelAgency::create(['name' => 'Adventure Tours', 'phone' => '0623456789', 'email' => 'contact@adventuretours.com']);
+        TravelAgency::create(['name' => 'Global Getaways', 'phone' => '0611223344', 'email' => 'support@globalgetaways.com']);
+        TravelAgency::create(['name' => 'Dream Destinations', 'phone' => '0633445566', 'email' => 'info@dreamdestinations.com']);
+        TravelAgency::create(['name' => 'Paradise Travel', 'phone' => '0644556677', 'email' => 'hello@paradisetravel.com']);
+        TravelAgency::create(['name' => 'Explorer Experts', 'phone' => '0655667788', 'email' => 'contact@explorerexperts.com']);
+        TravelAgency::create(['name' => 'City Breaks Co.', 'phone' => '0666778899', 'email' => 'info@citybreaks.com']);
+        TravelAgency::create(['name' => 'Luxury Escapes', 'phone' => '0677889900', 'email' => 'luxury@escapes.com']);
+        TravelAgency::create(['name' => 'Budget Travelers', 'phone' => '0688990011', 'email' => 'budget@travelers.com']);
+        TravelAgency::create(['name' => 'Nature Trails', 'phone' => '0612344321', 'email' => 'nature@trails.com']);
+        TravelAgency::create(['name' => 'Family Adventures', 'phone' => '0623455432', 'email' => 'family@adventures.com']);
+        TravelAgency::create(['name' => 'Honeymoon Heaven', 'phone' => '0634566543', 'email' => 'honeymoon@heaven.com']);
+        TravelAgency::create(['name' => 'Cultural Journeys', 'phone' => '0645677654', 'email' => 'cultural@journeys.com']);
+        TravelAgency::create(['name' => 'Island Hoppers', 'phone' => '0656788765', 'email' => 'island@hoppers.com']);
+        TravelAgency::create(['name' => 'Mountain Escapes', 'phone' => '0667899876', 'email' => 'mountain@escapes.com']);
+        TravelAgency::create(['name' => 'Desert Adventures', 'phone' => '0678900987', 'email' => 'desert@adventures.com']);
+        TravelAgency::create(['name' => 'Eco Travels', 'phone' => '0689012098', 'email' => 'eco@travels.com']);
+        TravelAgency::create(['name' => 'Urban Explorers', 'phone' => '0612131415', 'email' => 'urban@explorers.com']);
+        TravelAgency::create(['name' => 'Globetrotters', 'phone' => '0623242526', 'email' => 'globe@trotters.com']);
+        TravelAgency::create(['name' => 'World Wonders', 'phone' => '0634353637', 'email' => 'world@wonders.com']);
+        TravelAgency::create(['name' => 'Holiday Planners', 'phone' => '0645464748', 'email' => 'holiday@planners.com']);
+        TravelAgency::create(['name' => 'Weekend Getaways', 'phone' => '0656575859', 'email' => 'weekend@getaways.com']);
+        TravelAgency::create(['name' => 'Summer Escapes', 'phone' => '0667686969', 'email' => 'summer@escapes.com']);
+        TravelAgency::create(['name' => 'Winter Retreats', 'phone' => '0678797070', 'email' => 'winter@retreats.com']);
+        TravelAgency::create(['name' => 'Coastal Voyages', 'phone' => '0689808080', 'email' => 'coastal@voyages.com']);
+        TravelAgency::create(['name' => 'Exotic Adventures', 'phone' => '0610019191', 'email' => 'exotic@adventures.com']);
+        TravelAgency::create(['name' => 'Scenic Getaways', 'phone' => '0621129292', 'email' => 'scenic@getaways.com']);
+        TravelAgency::create(['name' => 'Hidden Gems Travel', 'phone' => '0632239393', 'email' => 'hidden@gems.com']);
+        TravelAgency::create(['name' => 'Wilderness Tours', 'phone' => '0643349494', 'email' => 'wilderness@tours.com']);
+        TravelAgency::create(['name' => 'Luxury Travels', 'phone' => '0654459595', 'email' => 'luxury@travels.com']);
+        TravelAgency::create(['name' => 'Group Adventures', 'phone' => '0665569696', 'email' => 'group@adventures.com']);
+        TravelAgency::create(['name' => 'Beachside Bliss', 'phone' => '0676679797', 'email' => 'beachside@bliss.com']);
+        TravelAgency::create(['name' => 'Nature Escapes', 'phone' => '0687789898', 'email' => 'nature@escapes.com']);
+        TravelAgency::create(['name' => 'Global Explorations', 'phone' => '0618899999', 'email' => 'global@explorations.com']);
+        TravelAgency::create(['name' => 'City Adventures', 'phone' => '0629900000', 'email' => 'city@adventures.com']);
+        TravelAgency::create(['name' => 'Eco Tours', 'phone' => '0631010101', 'email' => 'eco@tours.com']);
+        TravelAgency::create(['name' => 'Travel Titans', 'phone' => '0642121212', 'email' => 'travel@titans.com']);
+        TravelAgency::create(['name' => 'Destination Masters', 'phone' => '0653232323', 'email' => 'destination@masters.com']);
+        TravelAgency::create(['name' => 'Adventure Seekers', 'phone' => '0664343434', 'email' => 'adventure@seekers.com']);
+        TravelAgency::create(['name' => 'Wanderlust Tours', 'phone' => '0675454545', 'email' => 'wanderlust@tours.com']);
+        TravelAgency::create(['name' => 'Seaside Adventures', 'phone' => '0686565656', 'email' => 'seaside@adventures.com']);
+        TravelAgency::create(['name' => 'Horizon Travels', 'phone' => '0617676767', 'email' => 'horizon@travels.com']);
+        TravelAgency::create(['name' => 'Sky High Adventures', 'phone' => '0628787878', 'email' => 'skyhigh@adventures.com']);
+        TravelAgency::create(['name' => 'Ocean Explorers', 'phone' => '0639898989', 'email' => 'ocean@explorers.com']);
+        TravelAgency::create(['name' => 'Golden Horizons', 'phone' => '0640909090', 'email' => 'golden@horizons.com']);
+        TravelAgency::create(['name' => 'Wanderers Club', 'phone' => '0651010101', 'email' => 'wanderers@club.com']);
+        TravelAgency::create(['name' => 'Explorers Haven', 'phone' => '0662121212', 'email' => 'explorer@haven.com']);
+        TravelAgency::create(['name' => 'Adventurers Delight', 'phone' => '0673232323', 'email' => 'adventurer@delight.com']);
+    
     }
 }
-
